3cc3fbec63ada07b12bc594795ff5f44b2e7fd1c
am.soheili <am.soheili@mydigipay.com>
2024-10-29 23:16:49 +0330

    Feature: validations added to enter page

--- src/pages/enter/Enter.vue
+++ src/pages/enter/Enter.vue
@@ -2,12 +2,12 @@
   <div class="container">
     <div class="title">{{!isRegister ? 'LOGIN' : 'REGISTER'}}</div>
     <div class="forms">
-      <UiInput v-if="isRegister" label="User" :has-error="false" error-message="" @changeValue="setUser"></UiInput>
-      <UiInput label="Email" type="email" :has-error="false" error-message="" @changeValue="setEmail"></UiInput>
-      <UiInput label="Password" type="password" :has-error="false" error-message="" @changeValue="setPassword"></UiInput>
+      <UiInput v-if="isRegister" label="User" :has-error="!validity.user" :error-message="errors.user" @changeValue="setUser"></UiInput>
+      <UiInput label="Email" type="email" :has-error="!validity.email" :error-message="errors.email" @changeValue="setEmail"></UiInput>
+      <UiInput label="Password" type="password" :has-error="!validity.password" :error-message="errors.password" @changeValue="setPassword"></UiInput>
     </div>
     <div class="cta">
-      <UiButton :button-text="isRegister ? 'Register' : 'Login'"></UiButton>
+      <UiButton :button-text="isRegister ? 'Register' : 'Login'" @click="confirm"></UiButton>
     </div>
     <div class="change-side">
       <div class="question">
@@ -29,6 +29,59 @@ const isRegister = ref(false);
 const user = ref('');
 const email = ref('');
 const password = ref('');
+const errors = ref({
+  user: '',
+  email:'',
+  password:'',
+});
+const validity = ref({
+  user: true,
+  email: true,
+  password: true,
+});
+
+
+function confirm(){
+
+  updateErrorMessages();
+
+  if(!isFormValid()){
+    console.log('not valid')
+    return;
+  }
+
+}
+
+function isFormValid() {
+  if(isRegister.value){
+    return isUsernameValid() && isEmailValid() && isPasswordValid();
+  }
+
+  return  isEmailValid() && isPasswordValid();
+}
+
+function updateErrorMessages(){
+  errors.value.user = (isRegister.value && !isUsernameValid()) ? 'Enter user name is not valid' : '';
+
+  errors.value.email = !isEmailValid() ? 'Entered email is not valid' : '';
+
+  errors.value.password = !isPasswordValid() ? 'Enter password is not valid' : '';
+}
+
+function isEmailValid(){
+  validity.value.email = email.value && email.value.length > 0 && email.value.toString().includes('@')
+  return validity.value.email;
+}
+
+function isUsernameValid() {
+  validity.value.user = user.value.length > 0;
+  return validity.value.user;
+}
+
+function isPasswordValid() {
+  validity.value.password = password.value.length > 8;
+  return validity.value.password;
+}
 
 function setEmail(enteredEmail) {
   email.value = enteredEmail;
@@ -42,8 +95,37 @@ function setUser(enteredUser) {
   user.value = enteredUser;
 }
 
-function toggleMode(){
+function toggleMode() {
+  cleanErrors();
+  cleanInputs();
   isRegister.value = !isRegister.value;
+}
+
+function cleanInputs() {
+  user.value = '';
+  email.value = '';
+  password.value = '';
+}
+
+function cleanErrors() {
+  cleanErrorStatus();
+  cleanErrorMessages();
+}
+
+function cleanErrorStatus() {
+  validity.value = {
+    user: true,
+    email: true,
+    password: true,
+  }
+}
+
+function cleanErrorMessages() {
+  errors.value = {
+    user: '',
+    email: '',
+    password: '',
+  };
 }
 
 </script>
@@ -65,6 +147,13 @@ function toggleMode(){
 
     .forms {
       margin-top: 50px;
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+    }
+
+    .cta {
+      margin-top: 8px;
     }
 
     .change-side {
@@ -90,5 +179,11 @@ function toggleMode(){
 
   }
 
+  @media(max-width: 450px){
+    .container {
+      min-width: unset;
+      width: 100%;
+    }
+  }
 
 </style>
